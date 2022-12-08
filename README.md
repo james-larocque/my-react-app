628ddded86f2795e8a2e655df087c9f3e53e024a
James LaRocque <james.larocque@gmail.com>
2022-12-07 22:57:52 -0700

    Intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is based off this, https://www.educative.io/answers/how-to-create-a-react-app-without-using-create-react-app-cli
---
+++ src/components/Helloworld.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld = () => {
+    return ( 
+        <React.Fragment>
+         <h1> Hello World! </h1>
+        </React.Fragment>
+     );
+}
+ 
+export default HelloWorld;
