1b68880a48eb8a16d616086f840f8b30eaa47144
James LaRocque <james.larocque@gmail.com>
2022-12-07 23:55:01 -0700

    Added some styling
    Added Tailwind CSS to help with rapid styling of the application.

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const HelloWorld = () => {
-    return ( 
-         <h1> Hello World!! </h1>
-     );
+    return (
+        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-4 m-5">
+            <p className="text-3xl text-gray-700 font-bold mb-5">
+                Welcome!
+            </p>
+            <p className="text-gray-500 text-lg">
+                React and Tailwind CSS in action
+            </p>
+        </div>
+
+    );
 }
- 
+
 export default HelloWorld;
