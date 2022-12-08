fc449cd05e0f74fbb4437ee6a952375622fcf40e
James LaRocque <james.larocque@gmail.com>
2022-12-08 00:11:24 -0700

    Moved to TypeScript
    The Parcel bundler supports Typescript natively so it was just a matter
    of chaning the .js to .tsx in order to move everything to Typescript and
    get the benefits of developing in Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import HelloWorld from './components/HelloWorld'
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
     return (
@@ -6,7 +7,7 @@ function App() {
         <HelloWorld />
       </div>
     );
-  }
+  };
   
   export default App;
   
