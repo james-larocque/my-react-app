1295c5aec53bbb6e01b3191a4fd1eb99c0de7df7
James LaRocque <jlarocque@legalaid.ab.ca>
2022-12-13 14:20:21 -0700

    Sidebar remains static
    The sidebar does not disappear when it gets to a certain screen size as
    currently the sidebar is the main navigation at all form factors.

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -4,13 +4,13 @@ const Sidebar = () => {
     const [open, setOpen] = useState(false);
     return (
         <div className='min-h-0 h-full flex-1 flex overflow-hidden'>
-            <nav aria-label='Sidebar' className='narrow-sidebar hidden lg:block flex-shrink-0 bg-gray-800 overflow-y-auto'>
+            <nav aria-label='Sidebar' className='narrow-sidebar lg:block flex-shrink-0 bg-gray-800 overflow-y-auto'>
                 <div className='relative w-20 flex space-y-16 flex-col p-3'>
                     <a href="#" class="text-gray-400 hover:text-red-700">
                         <div class="flex-shrink-0 inline-flex items-center justify-center w-14">
                             <i class="fa fa-house"></i>
                         </div>
-                        <div class="text-center text-xs font-normal ">Home</div>
+                        <div class="text-center text-xs font-normal">Home</div>
                     </a>
 
                     <a href="#" class="text-gray-400 hover:text-red-700">
