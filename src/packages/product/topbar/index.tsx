6232d9f65a5a78e1ba48accbcf4e36fda13041c7
James LaRocque <james.larocque@gmail.com>
2024-07-23 18:48:08 -0600

    Paid down some technical debt

--- src/packages/product/topbar/index.tsx
+++ src/packages/product/topbar/index.tsx
@@ -32,7 +32,7 @@ const Topbar = () => {
                     {/* mobile button */}
                     <div className="md:hidden flex items-center">
                         <button onClick={() => mobileMenuVisibility === 'hidden' ? setMobileMenuVisibility('') : setMobileMenuVisibility('hidden')}>
-                            <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </button>
                     </div>
                 </div>
