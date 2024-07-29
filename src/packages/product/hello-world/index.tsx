e09350e9e9af1ca50826bf5ac36cb83b4e3989b1
James LaRocque <james.larocque@gmail.com>
2024-07-28 22:22:09 -0600

    Added an Azure Function

--- src/packages/product/hello-world/index.tsx
+++ src/packages/product/hello-world/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const HelloWorld = () => {
+
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        const url = '/api/GetHelloWorldMessage';
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const { text } = await response.json();
+                console.log(text);
+                setMessage(text);
+            } catch (error) {
+                console.log('error', error);
+            }
+        };
+
+        fetchData();
+
+    }, []);
+
     return (
         <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-4 m-5">
             <p className="text-3xl text-gray-700 font-bold mb-5">
                 Welcome!
             </p>
             <p className="text-gray-500 text-lg">
-                React and Tailwind CSS in action on Microsoft Azure Static Web Apps
-
-                So far, not so easy.
+                {message}
             </p>
         </div>
 
